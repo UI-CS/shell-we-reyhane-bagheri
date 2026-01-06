bcd13a7f3277d5e378e446c4086bfc75db9228d9
reyhane-bagheri <125139854+reyhane-bagheri@users.noreply.github.com>
2026-01-06 08:16:35 -0800

    Update main.c

--- main.c
+++ main.c
@@ -63,6 +63,62 @@ void *validate_subgrid(void *arg) {
             seen[num] = true;
         }
     }
+        
+    int subgrid_index = (p->row / SUBGRID_SIZE) * SUBGRID_SIZE + (p->column / SUBGRID_SIZE);
+    valid[2 + subgrid_index] = 1;
+    pthread_exit(NULL);
+}
+
+int run_sudoku(void) {
+    for (int i = 0; i < 11; i++) {
+        valid[i] = 1;
+    }
+
+    int sample_board[SIZE][SIZE] = {
+        {5, 3, 4, 6, 7, 8, 9, 1, 2},
+        {6, 7, 2, 1, 9, 5, 3, 4, 8},
+        {1, 9, 8, 3, 4, 2, 5, 6, 7},
+        {8, 5, 9, 7, 6, 1, 4, 2, 3},
+        {4, 2, 6, 8, 5, 3, 7, 9, 1},
+        {7, 1, 3, 9, 2, 4, 8, 5, 6},
+        {9, 6, 1, 5, 3, 7, 2, 8, 4},
+        {2, 8, 7, 4, 1, 9, 6, 3, 5},
+        {3, 4, 5, 2, 8, 6, 1, 7, 9}
+    };
+
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            board[i][j] = sample_board[i][j];
+        }
+    }
+
+    pthread_t threads[11];
+
+    pthread_create(&threads[0], NULL, validate_rows, NULL);
+    pthread_create(&threads[1], NULL, validate_cols, NULL);
+
+    for (int i = 0; i < 9; i++) {
+        sudoku_params *p = malloc(sizeof(sudoku_params));
+        p->row = (i / SUBGRID_SIZE) * SUBGRID_SIZE;
+        p->column = (i % SUBGRID_SIZE) * SUBGRID_SIZE;
+        pthread_create(&threads[2 + i], NULL, validate_subgrid, (void *)p);
+    }
+
+    for (int i = 0; i < 11; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    for (int i = 0; i < 11; i++) {
+        if (valid[i] == 0) {
+            printf("Sudoku is Invalid\n");
+            return 0;
+        }
+    }
+
+    printf("Sudoku is Valid\n");
+    return 0;
+}
+
     
 /* = UNIX SHELL = */
 static char last_command[MAX_LINE] = "";
